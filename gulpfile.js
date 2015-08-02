cf97b111e1bb297131e2d0fa5bec20e0ee1c8a18
Scarygami <scarygami@gmail.com>
2015-08-02 23:18:20 +0200

    Prepare chrome app for testing/demo

--- gulpfile.js
+++ gulpfile.js
@@ -8,19 +8,16 @@ var runSequence = require('run-sequence');
 var merge = require('merge-stream');
 var path = require('path');
 
-var destDir = 'demo-app';
-
 var elemDir = __dirname.split(path.sep).pop();
 
-// Copy All Files At The Root Level (app)
-gulp.task('copy', function () {
+// Copy custom element and dependencies
+var copyTask = function (destDir) {
   var element = gulp.src([
     './**',
     '!./bower_components/**',
-    '!./demo/**',
+    '!./chrome-app/**',
     '!./demo-app/**',
     '!./node_modules/**',
-    '!./test/**',
     '!./test-app/**',
     '!./bower.json',
     '!./gulpfile.js',
@@ -28,51 +25,49 @@ gulp.task('copy', function () {
     '!./package.json',
     '!./README.md'
   ], {nodir: true}).pipe($.if('*.html', $.crisper()))
-    .pipe(gulp.dest(path.join(destDir, 'elements', elemDir)));
+    .pipe(gulp.dest(path.join(destDir, 'components', elemDir)));
 
   var bower = gulp.src([
     'bower_components/**/*'
   ]).pipe($.if('*.html', $.crisper()))
-    .pipe(gulp.dest(path.join(destDir, 'elements')));
+    .pipe(gulp.dest(path.join(destDir, 'components')));
 
   return merge(element, bower)
     .pipe($.size({title: 'copy'}));
-});
+};
 
-gulp.task('copy:test', function () {
-  var chromeApp = gulp.src([
-    './test/chrome-app**'
+gulp.task('copy:test', copyTask.bind(null, 'test-app'));
+gulp.task('copy:demo', copyTask.bind(null, 'demo-app'));
+
+// Create Chrome App to test/demo the custom element
+var createAppTask = function (type, destDir) {
+  return gulp.src([
+    './chrome-app/**'
   ], {nodir: true})
+    .pipe($.replace('_element_', elemDir))
+    .pipe($.replace('_type_', type))
     .pipe(gulp.dest(destDir));
+};
 
-  var testFiles = gulp.src([
-    './test/**',
-    '!./test/chrome-app/**'
-  ], {nodir: true})
-    .pipe(gulp.dest(path.join(destDir, 'elements', elemDir, 'test')));
-
-  return merge(chromeApp, testFiles)
-    .pipe($.size({title: 'copy:test'}));
-});
+gulp.task('app:test', createAppTask.bind(null, 'test', 'test-app'));
+gulp.task('app:demo', createAppTask.bind(null, 'demo', 'demo-app'));
 
 // Clean Output Directory
 gulp.task('clean:test', del.bind(null, ['test-app']));
 gulp.task('clean:demo', del.bind(null, ['demo-app']));
 
-
 gulp.task('build:test', ['clean:test'], function (cb) {
-  destDir = 'test-app';
   runSequence(
-    'copy',
     'copy:test',
+    'app:test',
     cb
   );
 });
 
 gulp.task('build:demo', ['clean:demo'], function (cb) {
-  destDir = 'demo-app';
   runSequence(
-    'copy',
+    'copy:demo',
+    'app:demo',
     cb
   );
 });
